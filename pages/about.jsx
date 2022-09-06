8ca19d9ff629970aa55e1367b5ab171e861c6685
jacfpentester <jacfpentester@gmail.com>
2022-09-07 00:38:37 +0200

    primera commit

---
+++ pages/about.jsx
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+
+export default function about() {
+  return (
+    <Layout
+      title="About"
+      description="Sobre mi web"
+    >
+      <h1>About</h1>
+    </Layout>
+  );
+}
---
+++ pages/contact.jsx
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+
+export default function contact() {
+  return (
+    <Layout
+    title="Contacto"
+    description="Sobre Contactar"
+    >
+      <h1>Contact</h1>
+    </Layout>
+  );
+}
